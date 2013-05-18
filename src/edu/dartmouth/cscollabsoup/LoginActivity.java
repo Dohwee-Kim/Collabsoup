c1418f8280e92deea760c79903a348eaf241b32e
jazzrabbit <jazzrabbit@cs.dartmouth.edu>
2013-05-18 19:27:14 -0400

    basic intent implemented : from LoginActivity -> MainActivity

---
+++ src/edu/dartmouth/cscollabsoup/LoginActivity.java
@@ -0,0 +1,51 @@
+package edu.dartmouth.cscollabsoup;
+
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.Toast;
+import android.view.View;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.util.Log;
+
+
+public class LoginActivity extends Activity {
+
+	public static final int REQUEST_SIGNIN = 0;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		System.out.println();
+		setContentView(R.layout.activity_main);
+		// hi i am here 
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+		//betty is committing
+	}
+	
+	// **************** button click callback ********************** //
+	
+	public void onSigninClicked(View v){
+		Toast.makeText(getApplicationContext(),
+				"Testing..",
+				Toast.LENGTH_SHORT).show();
+		
+		// Later , need to implement putExtra for additional log in info  
+		Intent intent = new Intent(this, MainActivity.class);
+		try {
+			startActivity(intent);
+		}catch(ActivityNotFoundException e){
+			//Log.d("DEBUGHERE", "NOOOOO");
+			e.printStackTrace();
+		}
+		//startActivity(intent);
+	}
+}
--- src/edu/dartmouth/cscollabsoup/MainActivity.java
+++ src/edu/dartmouth/cscollabsoup/MainActivity.java
@@ -1,25 +1,26 @@
 package edu.dartmouth.cscollabsoup;
 
-import android.os.Bundle;
+
+import edu.dartmouth.cscollabsoup.R;
+import android.app.Fragment;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
 
 public class MainActivity extends Activity {
-
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
-		System.out.println();
-		setContentView(R.layout.activity_main);
-		// hi i am here 
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-		//betty is committing
-	}
-
+//	
+//	@Override
+//	public View onCreateView(LayoutInflater inflater, ViewGroup container, 
+//			Bundle savedInstanceState){
+//		// inflate layout here and return the View !!
+//		return inflater.inflate(R.layout.activity_profile, container, false);
+//	}
 }
