f50d07f8b723cfa9b96e799b755b0ad1f6496ff7
Sam <Sam@10.31.202.240>
2013-05-21 10:31:37 -0400

    Committing these guys, thought I did it last night..

---
+++ src/edu/dartmouth/cscollabsoup/AddCourses.java
@@ -0,0 +1,14 @@
+package edu.dartmouth.cscollabsoup;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddCourses extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.add_course);
+		
+	}
+}
---
+++ src/edu/dartmouth/cscollabsoup/CourseTabFragment.java
@@ -0,0 +1,20 @@
+package edu.dartmouth.cscollabsoup;
+
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+public class CourseTabFragment extends Fragment {
+	
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.courses, container, false);
+       
+    }
+
+}
