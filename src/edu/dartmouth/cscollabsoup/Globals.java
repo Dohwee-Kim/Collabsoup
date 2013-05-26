fd89af9489e85ff6243cb4a9701690f4fc4999d3
Jay Patel <jay.patel.gr@dartmouth.edu>
2013-05-25 21:09:31 -0400

    Pushing Globals and JSON file

---
+++ src/edu/dartmouth/cscollabsoup/Globals.java
@@ -0,0 +1,22 @@
+package edu.dartmouth.cscollabsoup;
+
+// All the global constants are put here
+public abstract class Globals 
+{
+
+	// Debugging tag for the whole project
+	public static final String TAG = "CollabSoup";
+	
+	
+	//IMP:Format tablename_KEY_columname
+
+	
+	// Profile Table!!! 
+	public static final String PROFILE_KEY_ROWID = "_id";
+	public static final String PROFILE_KEY_FIRSTNAME = "fname";
+	public static final String PROFILE_KEY_LASTNAME = "lname";
+	public static final String PROFILE_KEY_EMAIL = "email";
+	public static final String PROFILE_KEY_PASSWORD = "password";
+	public static final String SERVER_IP = "10.31.234.243:8080";
+
+}
