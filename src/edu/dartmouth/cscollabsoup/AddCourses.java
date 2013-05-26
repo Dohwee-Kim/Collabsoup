0cee9d9623951aec1ecf744073385655aa5fc6b7
Betty <Betty@10.31.227.248>
2013-05-26 03:35:16 -0400

    PULL NOW-incorporated hilton's GUI (because some of us can't push :P)

--- src/edu/dartmouth/cscollabsoup/AddCourses.java
+++ src/edu/dartmouth/cscollabsoup/AddCourses.java
@@ -1,14 +1,81 @@
 package edu.dartmouth.cscollabsoup;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class AddCourses extends Activity{
+
+	private static final String TAG = "COLLAB";
+	private Spinner mSpinnerDpmt;
+	private Spinner mSpinnerCourseNumber;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.add_course);
 		
+		mSpinnerDpmt = (Spinner) findViewById(R.id.dpmt_spinner);
+		mSpinnerCourseNumber = (Spinner) findViewById (R.id.course_spinner);
+		
+		ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String> (this, android.R.layout.simple_spinner_item, android.R.id.text1);
+
+		spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		mSpinnerCourseNumber.setAdapter(spinnerAdapter);
+
+		for (int i = 1; i <= 250; i++) spinnerAdapter.add(String.valueOf(i));
+		spinnerAdapter.notifyDataSetChanged();
 	}
+	
+	public void onAddCourseClicked(View v){
+  
+		String department = (String) mSpinnerDpmt.getSelectedItem();
+		String courseNumber = (String) mSpinnerCourseNumber.getSelectedItem();
+		
+		SharedPreferences s_pref = PreferenceManager.getDefaultSharedPreferences(v.getContext());
+		Editor edit=s_pref.edit();
+		
+		String course1Value = s_pref.getString("course1", "notset");
+		String course2Value = s_pref.getString("course2", "notset");
+		String course3Value = s_pref.getString("course3", "notset");
+		String course4Value = s_pref.getString("course4", "notset");
+
+		Log.d(TAG, course1Value);
+//		Log.d(TAG, course2Value);
+//		Log.d(TAG, course3Value);
+//		Log.d(TAG, course4Value);
+//		
+		if (course1Value.equals("notset"))
+		{
+			Log.d(TAG, "course 1 putString");
+			edit.putString("course1", department + " " + courseNumber);
+		}
+		else if (course2Value.equals("notset"))
+		{
+			Log.d(TAG, "course 2 putString");
+			edit.putString("course2", department + " " + courseNumber);
+		}
+		else if (course3Value.equals("notset"))
+		{
+			Log.d(TAG, "course 3 putString");
+			edit.putString("course3", department + " " + courseNumber);
+		}
+		else if (course4Value.equals("notset"))
+		{
+			Log.d(TAG, "course 4 putString");
+			edit.putString("course4", department + " " + courseNumber);
+		}
+
+        edit.commit();
+        
+		finish();
+    }
 }
--- src/edu/dartmouth/cscollabsoup/CourseTabFragment.java
+++ src/edu/dartmouth/cscollabsoup/CourseTabFragment.java
@@ -2,10 +2,15 @@ package edu.dartmouth.cscollabsoup;
 
 import android.app.Fragment;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Button;
 import android.content.Context;
@@ -13,7 +18,9 @@ import android.content.Context;
 
 public class CourseTabFragment extends Fragment {
 	private Context mContext;
-	private Button mButton;
+	private Button mButton, mAddCoursesButton;
+	private static final String TAG = "COLLAB";
+	private int first_check_flag = 0;
 	
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -22,6 +29,7 @@ public class CourseTabFragment extends Fragment {
         View view = inflater.inflate(R.layout.courses, container, false);
         
         mButton = (Button)view.findViewById(R.id.btnSync);
+        
         mButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Perform action on click
@@ -29,13 +37,72 @@ public class CourseTabFragment extends Fragment {
             }
             
         });
+        
+        
+        mAddCoursesButton = (Button) view.findViewById(R.id.coursePickerButton);
+        
+        Log.d(TAG, "setting on click Listener");
+        mAddCoursesButton.setOnClickListener(new View.OnClickListener() {
+ 			
+ 			@Override
+ 			public void onClick(View v) {
+ 				Intent intent = new Intent (getActivity(), AddCourses.class);
+ 				Log.d(TAG, "intent Created");
+ 				getActivity().startActivity(intent);
+ 				
+ 			}
+ 	});
         return view;
     }
+    
+
     
     public void onSyncClicked(View v){
 		// Later , need to implement putExtra for additional log in info  
 		Intent intent = new Intent(mContext, ParseDataHelper.class);
 		startActivity(intent);
     }
+    
+    @Override
+    public void onResume()
+    {
+    	SharedPreferences s_pref = PreferenceManager.getDefaultSharedPreferences(getActivity());
 
+    	TextView firstCourse = (TextView) getView().findViewById(R.id.first_course);
+		TextView secondCourse = (TextView) getView().findViewById(R.id.second_course);
+		TextView thirdCourse = (TextView) getView().findViewById(R.id.third_course);
+		TextView fourthCourse = (TextView) getView().findViewById(R.id.fourth_course);
+		
+		Log.d(TAG, firstCourse.getText().toString());
+		Log.d(TAG, secondCourse.getText().toString());
+		Log.d(TAG, thirdCourse.getText().toString());
+		Log.d(TAG, fourthCourse.getText().toString());
+		
+		if (first_check_flag == 0)
+		{
+			first_check_flag = 1;
+		}
+		else if (firstCourse.getText().toString().equals(" "))
+		{
+    		Log.d(TAG, "course 1 setText");
+    		firstCourse.setText(s_pref.getString("course1", "default text"));
+		}
+    	if (secondCourse.getText().toString().equals(" "))
+		{
+    		Log.d(TAG, "course 2 setText");
+    		secondCourse.setText(s_pref.getString("course2", " "));
+		}
+    	if (thirdCourse.getText().toString().equals(" "))
+		{
+    		Log.d(TAG, "course 3 setText");
+    		thirdCourse.setText(s_pref.getString("course3", " "));
+		}
+    	if (fourthCourse.getText().toString().equals(" "))
+		{
+    		Log.d(TAG, "course 4 setText");
+    		fourthCourse.setText(s_pref.getString("course4", " "));
+		}
+		
+        super.onResume();
+    }
 }
